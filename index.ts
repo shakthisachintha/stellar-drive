8c0fb0c0b1ad2305b44d600f54c2d451e1da5bc9
Shakthi Sachintha <43114582+shakthisachintha@users.noreply.github.com>
2022-12-26 20:05:12 +0530

    project initialized

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import fileRoutes from './routes/files';
+const app = express();
+
+app.use(express.json());
+
+// create a welcome route
+app.get("/", (req, res) => {
+    res.send("API is working!");
+});
+
+app.use('/files', fileRoutes)
+
+app.listen(3001, () => console.log("server started at 3001 port"));
---
+++ routes/files.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.send("list of files");
+});
+
+interface CreateFileDto{
+    title: string
+}
+
+router.post('/', (req, res) => {
+    const {title} = req.body as CreateFileDto; 
+    res.json(title);
+})
+
+export default router;
