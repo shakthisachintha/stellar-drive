79f76fd6544fb37767b89fee308f180ac4eccfd1
Shakthi Sachintha <43114582+shakthisachintha@users.noreply.github.com>
2022-12-28 11:59:21 +0530

    Added main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import DashboardPage from './pages/DashboardPage/DashboardPage';
+import LandingPage from './pages/LandingPage/LandingPage';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/dashboard",
+      element: <DashboardPage />,
+    },
+  ]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
