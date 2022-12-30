64ba2c0ccb4d7cbd00ee653e1bf3f8f112499405
Shakthi Sachintha <43114582+shakthisachintha@users.noreply.github.com>
2022-12-30 22:09:19 +0530

    component modofications for login

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,46 +1,78 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Header } from 'antd/es/layout/layout'
 import { NavLink } from 'react-router-dom'
 import './Navbar.scss'
 import { asset } from '../../assets'
 import { User } from '../../types'
+import { Subscription } from 'rxjs'
+import { AuthService } from '../../services/AuthService/AuthService'
 
 interface NavbarState {
   isLogged: boolean,
   user?: User
 }
 
-export default class Navbar extends Component<any, NavbarState>{
+export default class Navbar extends React.Component<any, NavbarState>{
+
+  private authStatusSubscription?: Subscription;
+
+  componentDidMount() {
+    this.authStatusSubscription = AuthService.getInstance().getLoggedInStateObserver().subscribe(authState => {
+      this.setState({
+        isLogged: authState.isLoggedIn,
+        user: {
+          userId: authState.user?.userId || "",
+          name: authState.user?.name || "",
+          token: authState.user?.token || ""
+        }
+      })
+    });
+    AuthService.getInstance().checkAuthStatus();
+  }
+
+  handleLogut() {
+    AuthService.getInstance().logout();
+  }
+
+  componentWillUnmount() {
+    console.log('unmount');
+    this.authStatusSubscription?.unsubscribe();
+  }
 
   constructor(props: any) {
     super(props);
-    this.state =  {isLogged: false };
+    this.state = { isLogged: false };
   }
 
   render() {
+    const { isLogged, user } = this.state;
     return (
       <Header className='navbar-component'>
         <div>
+          <NavLink to={'/'}>
           <div className='logo-container'>
             <img style={{ height: 40 }} src={asset.images.coverPng} alt="stellar drive logo" />
             <h2 style={{ color: "white", marginLeft: 20 }}>Stellar Drive</h2>
           </div>
+          </NavLink>
         </div>
 
         <div>
           <div className='nav-menu-container'>
-            <div className='nav-item'>
+            {!isLogged && <div className='nav-item'>
               <NavLink className='nav-link' to={'/login'}> Login </NavLink>
-            </div>
-            <div className='nav-item'>
-              <NavLink className='nav-link' to={'/dashboard'}> Dashboard </NavLink>
-            </div>
-            <div className='nav-item'>
+            </div>}
+            {isLogged && <div className='nav-item'>
+              <NavLink className='nav-link capitalized' to={'/dashboard'}> {user?.name} </NavLink>
+            </div>}
+            {isLogged && <div className='nav-item'>
               <NavLink className='nav-link' to={'/upload'}> Upload </NavLink>
-            </div>
+            </div>}
+            {isLogged && <div className='nav-item'>
+              <span onClick={this.handleLogut} className='nav-link'> Logout </span>
+            </div>}
           </div>
         </div>
-
       </Header>
     )
   }
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,163 @@
 import React, { Component } from 'react'
+import { Button, Form, Input, Tabs } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { AuthService } from '../../services/AuthService/AuthService';
 
 export default class LoginPage extends Component {
+
+  handleLogin() {
+    console.log('login');
+  }
+
+  handleRegister() {
+    console.log('register');
+  }
+
+  renderLogin() {
+
+    const onFinish = async (values: any) => {
+      try {
+        await AuthService.getInstance().login(values.username, values.password);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+      console.log('Failed:', errorInfo);
+    };
+
+    return (
+      <div>
+        <h1>Login</h1>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ username: "", password: "" }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          layout={'vertical'}
+        >
+          <Form.Item
+            name="username"
+            label="Username"
+            rules={[{ required: true, message: 'Please input your Username!' }]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your Password!' }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    )
+  }
+
+  renderRegister() {
+
+    const onFinish = async (values: any) => {
+      try {
+        AuthService.getInstance().register(values.username, values.password, values.name, values.email)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+      console.log('Failed:', errorInfo);
+    };
+
+    return (
+      <div>
+        <h1>Register</h1>
+        <Form
+          name="normal_login"
+          className="login-form"
+          autoComplete='off'
+          initialValues={{ name: "", username: "", password: "", confirmPassword: "", email: "" }}
+          aria-autocomplete='none'
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          layout={'vertical'}
+        >
+          <Form.Item
+            name="username"
+            label="Username"
+            rules={[{ required: true, message: 'Please pick a username!' }]}
+          >
+            <Input autoComplete="off" prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[{ required: true, message: 'Please enter an email address!' }, {type: 'email', message: 'Please enter a valid email!'}]}
+          >
+            <Input autoComplete="off" prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="name"
+            label="Name"
+            rules={[{ required: true, message: 'Please input your name!' }]}
+          >
+            <Input autoComplete="off" prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Name" />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please enter your password!' }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              autoComplete="off"
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item
+            label="Confirm Password"
+            name="confirmPassword"
+            rules={[{ required: true, message: 'Please type password again!' }]}
+          >
+            <Input
+              autoComplete="off"
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Confirm Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    )
+  }
+
   render() {
     return (
-      <div>LoginPage</div>
+      <Tabs
+        defaultActiveKey="1"
+        centered
+        items={[
+          { label: 'Login', key: 'login', children: this.renderLogin() },
+          { label: 'Register', key: 'register', children: this.renderRegister() }
+        ]}
+      />
     )
   }
 }
