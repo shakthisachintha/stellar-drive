fd6b845275e461bdf495d9f175b3476df2dcb4e2
sakunchamikara <ssakunchamikara@gmail.com>
2022-12-31 14:10:10 +0530

    updated the landing page to check the webhook

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class LandingPage extends Component {
   render() {
     return (
-      <div>Landing Page</div>
+      <div>Landing Page Edited</div>
     )
   }
 }
